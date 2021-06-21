31e95a75b2925c819860d4c64185dce1d1cf505a
yoralong <yoralong@gmail.com>
2021-06-21 22:28:44 +0700

    Add project files.

---
+++ Web Bán Quần Áo SecondHand/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Bán_Quần_Áo_SecondHand
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ Web Bán Quần Áo SecondHand/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_Bán_Quần_Áo_SecondHand.MvcApplication" Language="C#" %>
