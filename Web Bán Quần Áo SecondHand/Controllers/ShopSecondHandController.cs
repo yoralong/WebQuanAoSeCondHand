aac6e19342a427433629f710466e74adb027a056
yoralong <yoralong@gmail.com>
2021-06-25 10:06:35 +0700

    Chỉnh sữa , add SQL

--- Web Bán Quần Áo SecondHand/Controllers/ShopSecondHandController.cs
+++ Web Bán Quần Áo SecondHand/Controllers/ShopSecondHandController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_Bán_Quần_Áo_SecondHand.Models;
 
 namespace Web_Bán_Quần_Áo_SecondHand.Controllers
 {
     public class ShopSecondHandController : Controller
     {
         // GET: ShopSecondHand
+        dbQLShopSHDataContext data = new dbQLShopSHDataContext();
+
+        private List<SanPham> laysanpham (int count)
+		{
+            return data.SanPhams.OrderByDescending(a => a.NgayNhap).Take(count).ToList();
+		}
         public ActionResult Index()
         {
-            return View();
+            var sanphammoi = laysanpham(12);
+            return View(sanphammoi);
         }
+
     }
 }
