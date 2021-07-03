e8e8c5080391d9fc82c9b4e327d631d54db441e8
yoralong <yoralong@gmail.com>
2021-07-03 20:16:44 +0700

    klhác

--- Web Bán Quần Áo SecondHand/Controllers/ShopSecondHandController.cs
+++ Web Bán Quần Áo SecondHand/Controllers/ShopSecondHandController.cs
@@ -15,6 +15,7 @@ namespace Web_Bán_Quần_Áo_SecondHand.Controllers
         private List<SanPham> laysanpham (int count)
 		{
             return data.SanPhams.OrderByDescending(a => a.NgayNhap).Take(count).ToList();
+            //chao
 		}
         public ActionResult SPTheoMaLoai(string id)
         {
